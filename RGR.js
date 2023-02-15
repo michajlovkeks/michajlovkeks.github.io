88077dafa775125dd58f3ea8624666205695f928
michajlovkeks <michajlovkeks@gmail.com>
2023-02-15 21:57:04 +0700

    Add files via upload

--- RGR.js
+++ RGR.js
@@ -2,10 +2,14 @@ let btn = document.querySelector('.clr')
 let h = document.querySelector('.hex')
 let field = document.querySelector('.h')
 
+let r = document.querySelector('.r')
+let g = document.querySelector('.g')
+let b = document.querySelector('.b')
+
 const setCol = (e) => {
-    let r =+ document.querySelector('.r').value
-    let g =+ document.querySelector('.g').value
-    let b =+ document.querySelector('.b').value
+    let r = + document.querySelector('.r').value
+    let g = + document.querySelector('.g').value
+    let b = + document.querySelector('.b').value
     r = r.toString(16)
     g = g.toString(16)
     b = b.toString(16)
@@ -24,28 +28,46 @@ const setCol = (e) => {
 
 btn.addEventListener("click", setCol);
 
-let r = document.querySelector('.r')
-let g = document.querySelector('.g')
-let b = document.querySelector('.b')
-
-r.addEventListener('keyup', function(e) {
+r.addEventListener('keyup', function (e) {
     if (e.keyCode === 13) {
         document.querySelector('.clr').click();
     }
 });
 
-g.addEventListener('keyup', function(e) {
+g.addEventListener('keyup', function (e) {
     if (e.keyCode === 13) {
         document.querySelector('.clr').click();
     }
 });
 
-b.addEventListener('keyup', function(e) {
+b.addEventListener('keyup', function (e) {
     if (e.keyCode === 13) {
         document.querySelector('.clr').click();
     }
 });
+
+btn.onclick
 
 field.onclick = function (event) {
-    document.querySelector('.clr').click();  
+    document.querySelector('.clr').click();
+}
+
+field.oncontextmenu = function (event) {
+    let r = + document.querySelector('.r').value
+    let g = + document.querySelector('.g').value
+    let b = + document.querySelector('.b').value
+    r = r.toString(16)
+    g = g.toString(16)
+    b = b.toString(16)
+    if (r.length < 2) {
+        r = "0" + r;
+    }
+    if (g.length < 2) {
+        g = "0" + g;
+    }
+    if (b.length < 2) {
+        b = "0" + b;
+    }
+    document.querySelector('.txt').style.color = "#" + r + g + b;
+
 }
